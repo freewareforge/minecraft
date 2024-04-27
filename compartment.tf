3d0437efe9538f685039ffd72e29f7299d24716e
freewareforge <158120409+freewareforge@users.noreply.github.com>
2024-04-27 14:56:44 -0400

    Delete compartment.tf

--- compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "minecraft_compartment" {
-  name           = var.compartment_name
-  compartment_id = var.tenancy_ocid
-  description    = "Compartment created for Minecraft resources"
-  enable_delete  = true # Set to true to allow deletion of the compartment
-}
